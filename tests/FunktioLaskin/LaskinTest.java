a4b917970ce2598caa028173876511791c28bc53
Toni Kaija <toni.kaija@corefactory.com>
2016-09-15 14:57:29 +0300

    Add tests skeleton

---
+++ tests/FunktioLaskin/LaskinTest.java
@@ -0,0 +1,79 @@
+package FunktioLaskin;
+
+import org.junit.*;
+
+/**
+ * muas-sweng-project-2016
+ *
+ * @author diftraku
+ * @package muas-sweng-project-2016
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+public class LaskinTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void nollaa() throws Exception {
+
+    }
+
+    @Test
+    public void annaTulos() throws Exception {
+
+    }
+
+    @Test
+    public void lisaa() throws Exception {
+
+    }
+
+    @Test
+    public void vahenna() throws Exception {
+
+    }
+
+    @Test
+    public void kerro() throws Exception {
+
+    }
+
+    @Test
+    public void jaa() throws Exception {
+
+    }
+
+    @Test
+    public void nelio() throws Exception {
+
+    }
+
+    @Test
+    public void neliojuuri() throws Exception {
+
+    }
+
+    @Test
+    public void edellinen() throws Exception {
+
+    }
+
+    @BeforeClass
+    public void virtaON() throws Exception {
+
+    }
+
+    @AfterClass
+    public void virtaOFF() throws Exception {
+
+    }
+
+}
