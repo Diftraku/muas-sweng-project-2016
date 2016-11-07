52f252bda9173de3b8d1a928460fcab2bcac1949
Toni Kaija <toni.kaija@corefactory.com>
2016-11-07 11:44:49 +0200

    Refactor the GUI creation to prepare for testing

--- src/FunktioLaskin/Control.java
+++ src/FunktioLaskin/Control.java
@@ -1,191 +1,186 @@
 package FunktioLaskin;
 
-import java.util.ArrayList;
-
 /*
  * Controller class
  */
 public class Control {
+    private static final Control instance = new Control();
 
-	private Laskin laskin;
-	private Main main;
-	private Laskujarjestys laske;
-	private Listat lista;
-	private Listat lista1;
-	private Listat lista2;
-	private Listat lista3;
-	private double Tulos;
-	//private double vanha;
-	private int sulut = 0;
-	private double Valitulos;
-/*
- * Constructor
- */
-	public Control(){
-		laskin = new Laskin();
-		main = new Main();
-		laske = new Laskujarjestys();
-		lista = new Listat();
-		lista1 = new Listat();
-		lista2 = new Listat();
-		lista3 = new Listat();
-	}
-	/*
-	 * clears the calculator
-	 */
-	public void nollaa(){
-		//vanha = Tulos;
-		laskin.nollaa();
-		laske.nollaa();
-		Tulos = 0;
-		lista.nollaa();
-		lista1.nollaa();
-		lista2.nollaa();
-		lista3.nollaa();
-		sulut = 0;
-		Valitulos = 0.0;
-		//edellinen = "tyhja";
-		//arvot.clear();
-		//merkit.clear();
-		//miinus = 0;
-		//luku1 = 0;
-		//luku = null;
-	}
-	/*
-	 * Counts the calculation in the right order
-	 */
-	public void laskelopputulos(){
-		/*laske.setArvotList(lista.getArvotList());
+    private Laskin laskin;
+    private Main main;
+    private Laskujarjestys laske;
+    private Listat lista;
+    private Listat lista1;
+    private Listat lista2;
+    private Listat lista3;
+    private double Tulos;
+    //private double vanha;
+    private int sulut = 0;
+    private double Valitulos;
+
+    /*
+     * Constructor
+     */
+    private Control() {
+        laskin = new Laskin();
+        main = new Main();
+        laske = new Laskujarjestys();
+        lista = new Listat();
+        lista1 = new Listat();
+        lista2 = new Listat();
+        lista3 = new Listat();
+    }
+
+    public static Control getInstance() {
+        return instance;
+    }
+
+    /*
+     * clears the calculator
+     */
+    public void nollaa() {
+        //vanha = Tulos;
+        laskin.nollaa();
+        laske.nollaa();
+        Tulos = 0;
+        lista.nollaa();
+        lista1.nollaa();
+        lista2.nollaa();
+        lista3.nollaa();
+        sulut = 0;
+        Valitulos = 0.0;
+        //edellinen = "tyhja";
+        //arvot.clear();
+        //merkit.clear();
+        //miinus = 0;
+        //luku1 = 0;
+        //luku = null;
+    }
+
+    /*
+     * Counts the calculation in the right order
+     */
+    public void laskelopputulos() {
+        /*laske.setArvotList(lista.getArvotList());
 		laske.setMerkitList(lista.getMerkitList());*/
-		System.out.println("laskee lopputuloksen"+lista.getArvotList()+", "+lista.getMerkitList());
-		Tulos = laske.laske(lista.getArvotList(), lista.getMerkitList());
-	}
+        System.out.println("laskee lopputuloksen" + lista.getArvotList() + ", " + lista.getMerkitList());
+        Tulos = laske.laske(lista.getArvotList(), lista.getMerkitList());
+    }
 
-	public void setMerkki(String value){
-		System.out.println(sulut);
-		if (sulut == 0){
-			lista.setMerkki(value);
-		}
-		else if (sulut == 1){
-			lista1.setMerkki(value);
-		}
-		else if (sulut == 2){
-			lista2.setMerkki(value);
-		}
-		else if (sulut == 3){
-			lista3.setMerkki(value);
-		}
-	}
-	public void setLuku(String value){
-		System.out.println(sulut);
-		if (sulut == 0){
-			lista.setLuku(value);
-		}
-		else if (sulut == 1){
-			lista1.setLuku(value);
-		}
-		else if (sulut == 2){
-			lista2.setLuku(value);
-		}
-		else if (sulut == 3){
-			lista3.setLuku(value);
-		}
-	}
-	public void setEmerkki(String value){
-		System.out.println(sulut);
-		if (sulut == 0){
-			lista.setEmerkki(value);
-		}
-		else if (sulut == 1){
-			lista1.setEmerkki(value);
-		}
-		else if (sulut == 2){
-			lista2.setEmerkki(value);
-		}
-		else if (sulut == 3){
-			lista3.setEmerkki(value);
-		}
-	}
-	/*
-	 * Check if there are brackets (In progress)
-	 */
-	public void sulut(String sulku){
-		if (sulku == "("){
-			sulut++;
-		}
-		else if (sulku == ")"){
-			if (sulut == 1){
-				lista1.setMerkki("=");
-				laske.setEka(0);
+    public void setMerkki(String value) {
+        System.out.println(sulut);
+        if (sulut == 0) {
+            lista.setMerkki(value);
+        } else if (sulut == 1) {
+            lista1.setMerkki(value);
+        } else if (sulut == 2) {
+            lista2.setMerkki(value);
+        } else if (sulut == 3) {
+            lista3.setMerkki(value);
+        }
+    }
+
+    public void setLuku(String value) {
+        System.out.println(sulut);
+        if (sulut == 0) {
+            lista.setLuku(value);
+        } else if (sulut == 1) {
+            lista1.setLuku(value);
+        } else if (sulut == 2) {
+            lista2.setLuku(value);
+        } else if (sulut == 3) {
+            lista3.setLuku(value);
+        }
+    }
+
+    public void setEmerkki(String value) {
+        System.out.println(sulut);
+        if (sulut == 0) {
+            lista.setEmerkki(value);
+        } else if (sulut == 1) {
+            lista1.setEmerkki(value);
+        } else if (sulut == 2) {
+            lista2.setEmerkki(value);
+        } else if (sulut == 3) {
+            lista3.setEmerkki(value);
+        }
+    }
+
+    /*
+     * Check if there are brackets (In progress)
+     */
+    public void sulut(String sulku) {
+        if (sulku == "(") {
+            sulut++;
+        } else if (sulku == ")") {
+            if (sulut == 1) {
+                lista1.setMerkki("=");
+                laske.setEka(0);
 				/*laske.setArvotList(lista1.getArvotList());
 				laske.setMerkitList(lista1.getMerkitList());*/
-				Valitulos = laske.laske(lista1.getArvotList(), lista1.getMerkitList());
-				lista.addArvo(Valitulos);
-				System.out.println(Valitulos);
-				System.out.println(lista.getArvotList());
-				Valitulos = 0;
-				lista1.nollaa();
-				laske.nollaa();
-				laskin.nollaa();
-				laske.setEka(0);
-				lista.setEdellinenSulut();
+                Valitulos = laske.laske(lista1.getArvotList(), lista1.getMerkitList());
+                lista.addArvo(Valitulos);
+                System.out.println(Valitulos);
+                System.out.println(lista.getArvotList());
+                Valitulos = 0;
+                lista1.nollaa();
+                laske.nollaa();
+                laskin.nollaa();
+                laske.setEka(0);
+                lista.setEdellinenSulut();
 
-			}
-			else if (sulut == 2){
-				lista2.setMerkki("=");
-				laske.setEka(0);
-				//laske.setArvotList(lista2.getArvotList());
-				//laske.setMerkitList(lista2.getMerkitList());
-				//lista1.addArvo(laske.laske());
-				Valitulos = laske.laske(lista2.getArvotList(), lista2.getMerkitList());
-				lista1.addArvo(Valitulos);
-				lista2.nollaa();
-				laske.nollaa();
-				laskin.nollaa();
-				laske.setEka(0);
-				lista1.setEdellinenSulut();
-			}
-			else if (sulut == 3){
-				lista3.setMerkki("=");
-				laske.setEka(0);
-				//laske.setArvotList(lista3.getArvotList());
-				//laske.setMerkitList(lista3.getMerkitList());
-				//lista2.setLuku(Double.toString(laske.laske()));
-				Valitulos = laske.laske(lista3.getArvotList(), lista3.getMerkitList());
-				lista2.addArvo(Valitulos);
-				lista3.nollaa();
-				laske.nollaa();
-				laskin.nollaa();
-				laske.setEka(0);
-				lista2.setEdellinenSulut();
-			}
-			else{
-				 System.out.println("Shit happens");
-			}
-			sulut--;
-		}
-	}
+            } else if (sulut == 2) {
+                lista2.setMerkki("=");
+                laske.setEka(0);
+                //laske.setArvotList(lista2.getArvotList());
+                //laske.setMerkitList(lista2.getMerkitList());
+                //lista1.addArvo(laske.laske());
+                Valitulos = laske.laske(lista2.getArvotList(), lista2.getMerkitList());
+                lista1.addArvo(Valitulos);
+                lista2.nollaa();
+                laske.nollaa();
+                laskin.nollaa();
+                laske.setEka(0);
+                lista1.setEdellinenSulut();
+            } else if (sulut == 3) {
+                lista3.setMerkki("=");
+                laske.setEka(0);
+                //laske.setArvotList(lista3.getArvotList());
+                //laske.setMerkitList(lista3.getMerkitList());
+                //lista2.setLuku(Double.toString(laske.laske()));
+                Valitulos = laske.laske(lista3.getArvotList(), lista3.getMerkitList());
+                lista2.addArvo(Valitulos);
+                lista3.nollaa();
+                laske.nollaa();
+                laskin.nollaa();
+                laske.setEka(0);
+                lista2.setEdellinenSulut();
+            } else {
+                System.out.println("Shit happens");
+            }
+            sulut--;
+        }
+    }
 
-	public void setPii(){
-		if (sulut == 0){
-			lista.setPii();
-		}
-		else if (sulut == 1){
-			lista1.setPii();
-		}
-		else if (sulut == 2){
-			lista2.setPii();
-		}
-		else if (sulut == 3){
-			lista3.setPii();
-		}
-	}
-	/*
-	 * returns the result
-	 * @return Tulos Tulos is the result of the calculation
-	 */
-	public double getTulos(){
-		return Tulos;
-	}
+
+    public void setPii() {
+        if (sulut == 0) {
+            lista.setPii();
+        } else if (sulut == 1) {
+            lista1.setPii();
+        } else if (sulut == 2) {
+            lista2.setPii();
+        } else if (sulut == 3) {
+            lista3.setPii();
+        }
+    }
+
+    /*
+     * returns the result
+     * @return Tulos Tulos is the result of the calculation
+     */
+    public double getTulos() {
+        return Tulos;
+    }
 }
 
--- src/FunktioLaskin/Main.java
+++ src/FunktioLaskin/Main.java
@@ -1,157 +1,45 @@
 package FunktioLaskin;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-
-import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.*;
-
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.util.Objects;
 
 /*
  * Main class for starting the javafx application
  */
 public class Main extends Application {
-	private Control control;
-	private DoubleProperty value = new SimpleDoubleProperty();
-	private TextField screen;
-	private static final String[][] template = {
-			  { "^", "N", ".", "(", ")"},
-		      { "7", "8", "9", "/", "sin"},
-		      { "4", "5", "6", "*", "cos" },
-		      { "1", "2", "3", "-", "tan" },
-		      { "0", "c", "=", "+", "Pii" }
-		};
-/*
- * (non-Javadoc)
- * @see javafx.application.Application#start(javafx.stage.Stage)
- */
-	@Override
-	public void start(Stage stage) {
-		control = new Control();
-		final TextField screen  = createScreen();
-	    final TilePane  buttons = createButtons();
+    private Control control;
+    private VBox view;
+    private DoubleProperty value = new SimpleDoubleProperty();
 
-	    stage.setTitle("Calc");
-	    stage.initStyle(StageStyle.UTILITY);
-	    stage.setResizable(false);
-	    stage.setScene(new Scene(createLayout(screen, buttons)));
-	    stage.show();
-	}
-/*
- * Creates the calculator layout
- * @param screen the textfield where the numbers go
- * @param buttons Buttons TilePane where all the buttons are
- * @return Vbox which has the screen and buttons in it
- */
-	private VBox createLayout(TextField screen, TilePane buttons) {
-	    final VBox layout = new VBox(20);
-	    layout.setAlignment(Pos.CENTER);
-	    layout.setStyle("-fx-background-color: darkgray; -fx-padding: 20; -fx-font-size: 20;");
-	    layout.getChildren().setAll(screen, buttons);
-	    screen.prefWidthProperty().bind(buttons.widthProperty());
-	    return layout;
-	}
-	/*
-	 * Creates the screen
-	 * @return TextField Textfield is the screen where numbers go
-	 */
-	private TextField createScreen() {
-	    screen = new TextField();
-	    screen.setStyle("-fx-background-color: aquamarine;");
-	    screen.setAlignment(Pos.CENTER_RIGHT);
-	    screen.setEditable(false);
-	    return screen;
-	}
-	/*
-	 * Creates all the buttons from the template
-	 * @return TilePane Pane which has all the buttons in it
-	 */
-	private TilePane createButtons() {
-	    TilePane buttons = new TilePane();
-	    buttons.setVgap(7);
-	    buttons.setHgap(7);
-	    buttons.setPrefColumns(template[0].length);
-	    for (String[] r: template) {
-	      for (String s: r) {
-	        buttons.getChildren().add(createButton(s));
-	      }
-	    }
-	    return buttons;
-	}
-	/*
-	 * @param s Is the name of the button which comes from the template
-	 * @return Button returns JavaFX Button object
-	 */
-	private Button createButton(String s) {
-	    Button button = new Button(s);
-	    button.setStyle("-fx-base: beige;");
-	    button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-	    button.setOnAction(myHandler);
-	    return button;
-	}
+    /*
+     * Main function
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	final EventHandler<ActionEvent> myHandler = new EventHandler<ActionEvent>(){
-		/*
-		 * (non-Javadoc)
-		 * @see javafx.event.EventHandler#handle(javafx.event.Event)
-		 */
-        @Override
-        public void handle(final ActionEvent e) {
-        	Button button = (Button)e.getSource();
-        	String value = button.getText();
-        	if(value == "c") {
-        		screen.clear();
-        		control.nollaa();
-        	}
-        	else {
-        		if (value == "/" || value == "*" || value == "+" || value == "-" || value == "^") {
-        			control.setMerkki(value);
-        		}
-        		else if (value == "N" || value == "sin" || value == "tan" || value == "cos") {
-        			control.setEmerkki(value);
-        		}
-        		else if (value == "Pii"){
-        			control.setPii();
-        		}
-        		else if (value == "="){
-        			control.setMerkki(value);
-        			control.laskelopputulos();
-        			screen.setText(Double.toString(control.getTulos()));
-        			//control.nollaa();
-        		}
-        		else if (value == "0" || value == "1" || value == "2" || value == "3" || value == "4" || value == "5" || value == "6" || value == "7" || value == "8" || value == "9" || value == "."){
-        			control.setLuku(value);
-        		}
-        		else if (value == "(" || value == ")" ){
-        			control.sulut(value);
-
-        		}
-        		if(screen.getText() != null && !screen.getText().isEmpty()) {
-        			if (value != "="){
-        				screen.appendText(value);
-        			}
-        		}else {
-        			if (value != "="){
-        			screen.setText(value);
-        			}
-        		}
-        	}
-        }
-	};
-/*
- * Main function
- */
-	public static void main(String[] args) {
-		launch(args);
-	}
+    /*
+     * (non-Javadoc)
+     * @see javafx.application.Application#start(javafx.stage.Stage)
+     */
+    @Override
+    public void start(Stage stage) {
+        control = Control.getInstance();
+        view = new FunktioLaskinView();
+        stage.setTitle("Calc");
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setResizable(false);
+        stage.setScene(new Scene(view));
+        stage.show();
+    }
 }
