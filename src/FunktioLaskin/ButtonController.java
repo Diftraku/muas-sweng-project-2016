37d3344d1b4e02294a9f74044725a3aa800de095
Vili Ahlgren <ahlgrenvili@gmail.com>
2016-11-23 13:45:13 +0200

    Changes to Main and adding View.fxml as UI

---
+++ src/FunktioLaskin/ButtonController.java
@@ -0,0 +1,11 @@
+package FunktioLaskin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ButtonController {
+	@FXML
+	public void print(ActionEvent e){
+		System.out.println("Prints");
+	}
+}
--- src/FunktioLaskin/Main.java
+++ src/FunktioLaskin/Main.java
@@ -5,12 +5,15 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.util.Objects;
 
 /*
@@ -33,13 +36,13 @@ public class Main extends Application {
      * @see javafx.application.Application#start(javafx.stage.Stage)
      */
     @Override
-    public void start(Stage stage) {
-        control = Control.getInstance();
-        view = new FunktioLaskinView();
-        stage.setTitle("Calc");
-        stage.initStyle(StageStyle.UNIFIED);
-        stage.setResizable(false);
-        stage.setScene(new Scene(view));
-        stage.show();
+    public void start(Stage stage) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+		//FXMLLoader loader = new FXMLLoader(Main.class.getResource("View.fxml"));
+		//loader.setController(new MainController("FunktioLaskin.Control"));
+		//Parent root2 = loader.load();
+		stage.setTitle("Calculator");
+		stage.setScene(new Scene(root, 800, 700));	
+		stage.show();
     }
 }
