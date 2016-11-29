540bc87b3dffae50f55eed8e7459f94c736e4c61
Toni Kaija <toni.kaija@corefactory.com>
2016-11-29 18:27:49 +0200

    Use Controller instead of Control

--- tests/ControllerTest.java
+++ tests/ControllerTest.java
@@ -1,15 +1,15 @@
-import FunktioLaskin.Control;
-import FunktioLaskin.Laskin;
+import FunktioLaskin.Controller;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by mako on 11/29/16.
  */
-public class ControlTest {
-    private static Control controller = Control.getInstance();
+public class ControllerTest {
+    private static Controller controller = new Controller();
     @Before
     public void setUp() throws Exception {
 
